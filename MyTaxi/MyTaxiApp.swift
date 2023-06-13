d60bc9ce3cbdcfc79604c3f931b1ae8b3c70a56b
GirishCloudanalogy <girish.dadhich@cloudanalogy.com>
2023-06-13 16:16:17 +0530

    Initial Commit

---
+++ MyTaxi/MyTaxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTaxiApp.swift
+//  MyTaxi
+//
+//  Created by Girish Dadhich on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MyTaxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
